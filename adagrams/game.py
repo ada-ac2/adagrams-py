a7c3bb6ea7ce2a20ed83427248fcb998b3a7d1c9
Catherine Bandarchuk <kate@sealucky.com>
2022-10-26 22:27:48 -0700

    implement get_highest_word_score(word_list). Test_wave_04.py works.

--- adagrams/game.py
+++ adagrams/game.py
@@ -34,14 +34,14 @@ def draw_letters():
 
 def uses_available_letters(word, letter_bank):
     checked_word = list()
-    word_list = []
-    word_list[:] = word.upper()
-    for letter in word_list:
+    word_ls = []
+    word_ls[:] = word.upper()
+    for letter in word_ls:
         if letter in letter_bank:
             checked_word.append(letter)
             index = letter_bank.index(letter)
             del letter_bank[index]
-            if checked_word == word_list:
+            if checked_word == word_ls:
                 letter_bank.extend(checked_word)
                 letter_bank.sort()
                 return True
@@ -49,15 +49,42 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     user_points = 0
-    word_list = []
-    word_list[:] = word.upper()
-    if len(word_list)>= 7:
+    word_ls = []
+    word_ls[:] = word.upper()
+    if len(word_ls)>= 7:
         user_points = 8
     for element in SCORE_CHART:
-        for letter in word_list:
+        for letter in word_ls:
             if letter in SCORE_CHART[element]:
                 user_points += element
     return user_points
 
 def get_highest_word_score(word_list):
-    pass
+    users_score = 0
+    max_score = 0
+    max_word = ''
+    for word in word_list:
+        users_score = score_word(word)
+        if users_score > max_score:
+            max_score = users_score
+            max_word = word
+        elif users_score == max_score:
+            if len(word) < len(max_word):
+                if len(max_word) == 10:
+                    continue
+                else:
+                    max_score = users_score
+                    max_word = word
+            elif len(word) == len(max_word):
+                word_index = word_list.index(word)
+                max_word_index = word_list.index(max_word)
+                if word_index > max_word_index:
+                    continue
+                else:
+                    max_word = word
+                    max_score = users_score
+            elif len(word) == 10:
+                max_word = word
+                max_score = users_score
+    best_word = (max_word, max_score)
+    return best_word  # tuple of winning word and it's scores  best_word[ 0 - word , 1 - it's score]
