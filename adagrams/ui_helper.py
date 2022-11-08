383f7b6db03f0c9cc3b6c8b663ec6b9843b7a9fa
Yufei Bao <ybao1005@gmail.com>
2022-11-08 15:22:57 -0800

    Completed Adagrams Project

--- adagrams/ui_helper.py
+++ adagrams/ui_helper.py
@@ -1,19 +1,24 @@
 def display_welcome_message():
     print("Welcome to Adagrams!")
 
+
 def display_drawn_letters(letters):
     print("You have drawn the letters:")
     print(', '.join(letters))
 
+
 def display_game_instructions():
     print("Please input your submission for the longest anagram you can come up with")
+
 
 def display_needs_valid_input_message():
     print("You entered in a word that contains characters not in the letter bank")
     display_game_instructions()
 
+
 def display_score(score):
     print(f"Your submitted anagram scored {score} points")
+
 
 def display_retry_instructions():
     print("Should we play another round?")
@@ -25,6 +30,9 @@ def display_goodbye_message():
 
 # word_score must be a data structure such as a list or tuple where the first element
 # is the word and the second element is the score.
+
+
 def display_highest_score(word_score):
     print("Thanks for playing Adagrams!")
-    print(f"The highest score from this game was {word_score[0]}, which was worth {word_score[1]} points.")
+    print(
+        f"The highest score from this game was {word_score[0]}, which was worth {word_score[1]} points.")
